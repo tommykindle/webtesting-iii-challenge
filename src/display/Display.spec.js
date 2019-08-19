9699d4f6b9e5238b98ba497971291fc045151fcd
Tommy <tommykindle83@gmail.com>
2019-08-19 14:39:33 -0700

    Display render test, value test and ClassName test all working

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,16 @@
-// Test away!
+import React from 'react'
+import { render } from '@testing-library/react'
+import Display from './Display'
+
+
+describe('<Display />', () => {
+  it('renders', () => {
+    render(<Display />)
+  })
+  it('displays expected value', () => {
+    render(<Display closed={true} locked={false} />)
+  })
+  it('logic is working', () => {
+    render(<Display closed={true} locked={true} ClassName="red-led" />)
+  })
+})
