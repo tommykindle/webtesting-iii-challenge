47f20874bb19b513302c077aae495eb753e6faf3
Tommy <tommykindle83@gmail.com>
2019-08-19 15:40:44 -0700

    currently stuck on controls going to research more and come back to it

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,10 @@
 // Test away!
+import React from 'react'
+import { render } from '@testing-library/react'
+import Controls from './Controls'
+
+describe('<Controls />', () => {
+  it('renders', () => {
+    render(<Controls />)
+  })
+})
